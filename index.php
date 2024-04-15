df4a6116838e102b104ac7823315604f1d64c387
alfocea <alfocea@coches.com>
2024-04-15 15:25:34 +0200

    Second exercice fix

--- index.php
+++ index.php
@@ -34,56 +34,98 @@ class HelloTicketsAPI
     }
 }
 
+class CSVExporter 
+{
+    public static function export(array $header, array $content, string $filename)
+    {
+        ob_start();
+        header('Content-Type: text/csv; charset=utf-8');
+        header('Content-Disposition: attachment; filename='.$filename.'.csv');
+        header('Pragma: no-cache');
+        header('Expires: 0');
+
+        ob_end_clean();
+        $output = fopen('php://output', 'w');
+        fputcsv($output, $header);
+
+        foreach($content as $item) {
+            fputcsv($output, $item);
+        }
+
+        fclose($output);
+    }
+}
+
+class EventDataTransformer
+{
+    public static function e1FromRawToCSV($eventList): array
+    {
+        $output = [];
+
+        foreach ($eventList as $event) {
+            $eventVanue = $event['venue'];
+            $eventCategory = $event['category'];
+        
+            $output[] = [
+                $event['name'],
+                $event['url'],
+                $eventVanue['name'],
+                $eventCategory['name']
+            ];
+        }
+
+        return $output;
+    }
+
+    public static function e2FromRawToCSV($eventList): array
+    {
+        $output = [];
+
+        foreach ($eventList as $event) {
+            $output[] = [
+                $event['id'],
+                $event['name']
+            ];
+        }
+
+        return $output;
+    }
+}
+
 $api = new HelloTicketsAPI();
 
 // Excercise 1
-$rawEventList = $api->callAPI('events', [
+$rawAllNycEventList = $api->callAPI('events', [
     'city_id' => 1, //NYC
     'limit' => 0,
     'page' => 0
 ]);
 
+CSVExporter::export(
+    ['Event', 'URL', 'Venue', 'Category'],
+    EventDataTransformer::e1FromRawToCSV($rawAllNycEventList['events']),
+    'exercise1'
+);
+
 // Exercise 2
-$performances = $api->callAPI('extend/data/events', [
+$exercise2rawData = $api->callAPI('extend/data/events', [
     'city_id' => 1, //NYC
     'category_id' => 3, // Theatre
     'start_date' => '2024-05-20',
     'end_date' => '2024-06-01',
-    'limit' => 10,
-    'page' => 1
+    'limit' => 0,
+    'page' => 0
 ]);
 
-// Pending to refactor and move to class
-ob_start();
-header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename=exercise1.csv');
-header('Pragma: no-cache');
-header('Expires: 0');
+$performanceWithPriceList = array_filter($exercise2rawData['events'], function($event) {
+    $eventPerformanceList = $event['performances'];
+    foreach ($eventPerformanceList as $eventPerformance) {
+        return $eventPerformance['price_range']['min_price'] < 120;
+    }
+});
 
-$eventList = $rawEventList['events'];
-
-$csvData = [];
-
-foreach ($eventList as $event) {
-    $eventVanue = $event['venue'];
-    $eventCategory = $event['category'];
-
-    $csvData[] = [
-        $event['name'],
-        $event['url'],
-        $eventVanue['name'],
-        $eventCategory['name']
-    ];
-}
-
-ob_end_clean();
-$csvHeader = ['Event', 'URL', 'Venue', 'Category'];
-$output = fopen('php://output', 'w');
-fputcsv($output, $csvHeader);
-
-foreach($csvData as $item) {
-    fputcsv($output, $item);
-}
-
-fclose( $output );
-exit;
+CSVExporter::export(
+    ['Id', 'Name'],
+    EventDataTransformer::e2FromRawToCSV($performanceWithPriceList),
+    'exercise2'
+);
